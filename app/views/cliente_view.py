913e52baf663bda819c439570fb48723e6cbd331
Nathan Gabriel Muniz da Silva <natangabriel.palmerias@gmail.com>
2021-02-19 13:11:59 -0300

    Criei a view

--- app/views/cliente_view.py
+++ app/views/cliente_view.py
@@ -1,5 +1,13 @@
 from app import app
 
-@app.route('/ola')
-def ola():
-    return 'olá, mundo em Flask!'
+@app.route('/ola', defaults={'nome': None}, methods={"POST", "GET"})
+@app.route('/ola/<string:nome>')
+def ola(nome):
+    if nome:
+        return f'olá, {nome}'
+    else:
+        return f'Olá, usuario'
+
+@app.route('/')
+def home():
+    return "Hello World"
